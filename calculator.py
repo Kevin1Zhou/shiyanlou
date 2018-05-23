7ba03f1d962a1d11db58d887ae4157bde67bc08d
Kevin1Zhou <zk_212@163.com>
2018-05-23 10:31:45 +0800

    after all debugs

--- calculator.py
+++ calculator.py
@@ -20,19 +20,19 @@ def Cal_incoming_tax(salary,fee,start_point=3500): #计算应纳税所得额
 def Cal_tax(incoming_tax): #计算应纳税额
 	tax = 0	
 	if incoming_tax <= 1500:
-		tax = format(incoming_tax*0.03-0, ".2f")
+		tax = incoming_tax*0.03-0
 	elif incoming_tax > 1500 and incoming_tax <=4500:
-		tax = format(incoming_tax*0.1-105, ".2f")
+		tax = incoming_tax*0.1-105
 	elif incoming_tax > 4500 and incoming_tax <=9000:               
-                tax = format(incoming_tax*0.2-555, ".2f")
+                tax = incoming_tax*0.2-555
 	elif incoming_tax > 9000 and incoming_tax <=35000:               
-                tax = format(incoming_tax*0.25-1005, ".2f")
+                tax = incoming_tax*0.25-1005
 	elif incoming_tax > 35000 and incoming_tax <=55000:               
-                tax = format(incoming_tax*0.3-2755, ".2f")
+                tax = incoming_tax*0.3-2755
 	elif incoming_tax > 55000 and incoming_tax <=80000:               
-                tax = format(incoming_tax*0.35-5505, ".2f")
+                tax = incoming_tax*0.35-5505
 	else:
-		tax = format(incoming_tax*0.45-13505, ".2f")
+		tax = incoming_tax*0.45-13505
 	return tax
 	
 def Cal_salary_after_tax(salary,fee,tax):	#计算税后工资
@@ -40,14 +40,14 @@ def Cal_salary_after_tax(salary,fee,tax):	#计算税后工资
 	return salary_after_tax
 
 if __name__ == '__main__':
+	num = 0
 	for salary in Salarys:
-		num = 0
 		fee = Cal_fee(salary)
 		incoming_tax = Cal_incoming_tax(salary,fee)
 		tax = Cal_tax(incoming_tax)
 		salary_after_tax = Cal_salary_after_tax(salary,fee,tax)	
 		print(IDs[num], end=':')
-		print(salary_after_tax)
+		print(format(salary_after_tax, '.2f'))
 		num += 1
 
 
