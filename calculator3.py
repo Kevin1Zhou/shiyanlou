60a85d57eed241f1305b45a7a401b6bb7d0a9858
Kevin1Zhou <zk_212@163.com>
2018-05-25 11:20:03 +0800

    undone

--- calculator3.py
+++ calculator3.py
@@ -3,6 +3,15 @@
 import sys
 import csv
 
+class Args(object):
+	def __init__(self):
+		self.args = sys.argv[1:]
+	def get_content_of_option(self, option):
+		self.index = self.args.index(option)
+		self.content = self.args[self.index+1]
+		return self.content
+args = Args()
+
 class Config(object):
 	def __init__(self, configfile):
 		self._config = {}
@@ -12,7 +21,7 @@ class Config(object):
 				for i, value in enumerate(temp):
 					temp[i] = value.strip()
 				try:
-					self._config[temp[0]] = int(temp[1])
+					self._config[temp[0]] = float(temp[1])
 				except:
 					print("Parameter Error in Config File")
 	def get_config(self, dataname):
@@ -25,10 +34,11 @@ class UserData(object):
 			for line in file:
 				temp = line.strip().split(',')
 				try:
-					self._userdata[int(temp[0])] = int(tempt[1])
+					self._userdata[int(temp[0])] = int(temp[1])
 				except:
 					print("Parameter Error in Userdata file")
 	def get_userdata(self):
 		return self._userdata
 				
-
+class Calculator(object):
+	
